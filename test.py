e7017e6786e1017946ac415388eff69dd1b9828b
ScottSWu <scott.wu.hjx@gmail.com>
2017-02-27 16:32:39 -0500

    Update test script and test folder

--- test.py
+++ test.py
@@ -8,12 +8,17 @@ def checkEqual(f1, f2):
 if __name__=="__main__":
     TEST_ENCODED = "data/encoded.data"
     TEST_DECODED = "data/decoded.data"
-    TEMP_OUTPUT = "tmp.data"
+    TEMP_OUTPUT = "tmp/data"
 
     implementations = [
         { # javascript
             "name": "JavaScript",
             "execArgs": ["node", "src/R2ProtocolTest.js"]
+        },
+        { # C++
+            "name": "C++",
+            "setupArgs": ["g++", "-std=c++11", "src/R2ProtocolTest.cpp", "-I", "src", "-o", "tmp/R2Protocol.exe"],
+            "execArgs": ["tmp/R2Protocol.exe"]
         },
         #{ # python
         #    "execArgs": ["python", "src/r2interface-test.py"]
@@ -23,9 +28,10 @@ if __name__=="__main__":
     for impl in implementations:
         # Setup the implementation (e.g. compile)
         if "setupArgs" in impl:
-            subprocess.call(impl.setupArgs)
+            subprocess.call(impl["setupArgs"])
 
         # Run tests
+        print("Testing {}".format(impl["name"]))
         subprocess.call(impl["execArgs"] + ["decode", TEST_ENCODED, TEMP_OUTPUT])
         assert(checkEqual(TEST_DECODED, TEMP_OUTPUT))
         subprocess.call(impl["execArgs"] + ["encode", TEST_DECODED, TEMP_OUTPUT])
