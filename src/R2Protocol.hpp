7e4721908f0bf196154aa5498fecd2773695afea
Scott Wu <scott.wu.hjx@gmail.com>
2017-04-27 12:46:38 -0400

    Fix C++ decoding with multiple consecutive inputs

--- src/R2Protocol.hpp
+++ src/R2Protocol.hpp
@@ -22,9 +22,9 @@ namespace R2Protocol {
         char hex[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
         char digits[] = {
             hex[(checksum >> 12) & 0xf],
-            hex[(checksum >>  8) & 0xf],
-            hex[(checksum >>  4) & 0xf],
-            hex[(checksum >>  0) & 0xf]
+            hex[(checksum >> 8) & 0xf],
+            hex[(checksum >> 4) & 0xf],
+            hex[(checksum >> 0) & 0xf]
         };
         return std::string(digits);
     }
@@ -35,8 +35,8 @@ namespace R2Protocol {
     inline uint16_t computeChecksum(std::vector<uint8_t>& data, uint32_t start, uint32_t end) {
         uint32_t index;
         uint32_t sum = 0;
-        for (index = start; index < end - 1; index+=2) {
-            sum += (data[index] << 8) | data[index+1];
+        for (index = start; index < end - 1; index += 2) {
+            sum += (data[index] << 8) | data[index + 1];
             if (sum > 0xffff) {
                 sum -= 0xffff;
             }
@@ -47,13 +47,13 @@ namespace R2Protocol {
                 sum -= 0xffff;
             }
         }
-        return (uint16_t) (sum & 0xffff);
+        return (uint16_t)(sum & 0xffff);
     }
 
     inline uint32_t readString(std::vector<uint8_t>& buf, uint32_t index, uint32_t len, std::string& value) {
         std::vector<char> str(len);
         for (uint32_t i = 0; i < len; i++) {
-            str[i] = (char) buf[i + index];
+            str[i] = (char)buf[i + index];
         }
         value = std::string(str.begin(), str.end());
         return len;
@@ -73,12 +73,12 @@ namespace R2Protocol {
     }
 
     inline uint32_t readShort(std::vector<uint8_t>& buf, uint32_t index, uint16_t& value) {
-        value = (buf[index] << 8) | buf[index+1];
+        value = (buf[index] << 8) | buf[index + 1];
         return 2;
     }
 
     inline uint32_t readInt(std::vector<uint8_t>& buf, uint32_t index, uint32_t& value) {
-        value = buf[index] | (buf[index+1] << 8) | (buf[index+2] << 16) | (buf[index+3] << 24);
+        value = buf[index] | (buf[index + 1] << 8) | (buf[index + 2] << 16) | (buf[index + 3] << 24);
         return 4;
     }
 
@@ -87,10 +87,10 @@ namespace R2Protocol {
      */
     inline int32_t decode(std::vector<uint8_t>& input, Packet& params, bool skip_checksum = 0) {
         uint32_t start = 0;
-        while (start < input.size() - 3 && !(input[start] == 'G' && input[start + 1] == '0' && input[start + 2] == '0')) {
+        while (start + 3 < input.size() && !(input[start] == 'G' && input[start + 1] == '0' && input[start + 2] == '0')) {
             start++;
         }
-        if (start >= input.size() - 3) {
+        if (start + 3 >= input.size()) {
             return -1;
         }
 
@@ -120,7 +120,7 @@ namespace R2Protocol {
             }
             else if (key == 'K') {
                 uint16_t checksum;
-                uint16_t computedChecksum = computeChecksum(input, start, index-1);
+                uint16_t computedChecksum = computeChecksum(input, start, index - 1);
                 uint8_t len;
                 index += readByte(input, index, len);
                 index += readShort(input, index, checksum);
@@ -131,19 +131,24 @@ namespace R2Protocol {
                 params.checksum = checksumToHex(checksum);
             }
             else if (key == 'G') {
-                if (index + 2 < input.size() && input[index + 1] == '0' && input[index + 2] == '1') {
+                if (index + 1 < input.size() && input[index] == '0' && input[index + 1] == '1') {
                     index += 2;
                     end = 1;
                 }
             }
         }
-        return index;
+        if (end) {
+            return index;
+        }
+        else {
+            return -1;
+        }
     }
 
     inline uint32_t writeString(std::vector<uint8_t>& buf, uint32_t index, std::string str) {
-        uint32_t l = (uint32_t) str.length();
+        uint32_t l = (uint32_t)str.length();
         for (uint32_t i = 0; i < l; i++) {
-            buf[i + index] = (uint8_t) str[i];
+            buf[i + index] = (uint8_t)str[i];
         }
         return l;
     }
@@ -154,7 +159,7 @@ namespace R2Protocol {
     }
 
     inline uint32_t writeBytes(std::vector<uint8_t>& buf, uint32_t index, std::vector<uint8_t>& value) {
-        uint32_t l = (uint32_t) value.size();
+        uint32_t l = (uint32_t)value.size();
         for (uint32_t i = 0; i < l; i++) {
             buf[i + index] = value[i];
         }
@@ -162,10 +167,10 @@ namespace R2Protocol {
     }
 
     inline uint32_t writeInt(std::vector<uint8_t>& buf, uint32_t index, uint32_t value) {
-        buf[index  ] = (value >>  0) & 0xff;
-        buf[index+1] = (value >>  8) & 0xff;
-        buf[index+2] = (value >> 16) & 0xff;
-        buf[index+3] = (value >> 24) & 0xff;
+        buf[index] = (value >> 0) & 0xff;
+        buf[index + 1] = (value >> 8) & 0xff;
+        buf[index + 2] = (value >> 16) & 0xff;
+        buf[index + 3] = (value >> 24) & 0xff;
         return 4;
     }
 
@@ -173,7 +178,7 @@ namespace R2Protocol {
      * Encode the data, returning the size of the encoded data, or negative if data failed to be encoded
      */
     inline int32_t encode(Packet params, std::vector<uint8_t>& output) {
-        uint32_t len = (uint32_t) (
+        uint32_t len = (uint32_t)(
             3 + // G00 start
             2 + // S{length1} source
             params.source.length() + // source
@@ -189,16 +194,16 @@ namespace R2Protocol {
         uint32_t index = 0;
         index += writeString(output, index, "G00");
         index += writeString(output, index, "S");
-        index += writeByte(output, index, (uint8_t) params.source.length());
+        index += writeByte(output, index, (uint8_t)params.source.length());
         index += writeString(output, index, params.source);
         index += writeString(output, index, "D");
-        index += writeByte(output, index, (uint8_t) params.destination.length());
+        index += writeByte(output, index, (uint8_t)params.destination.length());
         index += writeString(output, index, params.destination);
         index += writeString(output, index, "T");
-        index += writeByte(output, index, (uint8_t) params.id.length());
+        index += writeByte(output, index, (uint8_t)params.id.length());
         index += writeString(output, index, params.id);
         index += writeString(output, index, "P");
-        index += writeInt(output, index, (uint32_t) params.data.size());
+        index += writeInt(output, index, (uint32_t)params.data.size());
         index += writeBytes(output, index, params.data);
         uint16_t checksum = computeChecksum(output, 0, index);
         index += writeString(output, index, "K");
